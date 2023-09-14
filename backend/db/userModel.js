9c12cc0d8eb23ab2ea18dbcf0e7fb4d482ce7192
Keshaw08 <keshawsoni08@gmail.com>
2023-09-14 19:38:42 +0530

    added backend files

---
+++ backend/db/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// user schema
+const UserSchema = new mongoose.Schema({
+  // name field
+  name:{
+    type:String,
+    required:[true, "Please provide a Name!"],
+    unique: false,
+  },
+
+  // email field
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+
+  //   password field
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
+});
+
+// export UserSchema
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
