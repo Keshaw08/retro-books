aa02179ca80cab655ce6909d85929ec4ed884d34
Keshaw08 <keshawsoni08@gmail.com>
2023-09-23 15:22:52 +0530

    added sidebar

--- retro-books/src/Components/Pages/FirstPage.js
+++ retro-books/src/Components/Pages/FirstPage.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Topbar from "../Topbar/Topbar";
+import Cards from "../Book_Cards/Cards";
+import "./FirstPage.css";
+import Sidebar from "../Sidebar/Sidebar";
 
 export default function FirstPage() {
   return (
     <div>
       <Topbar />
-      <h1>First Page Catalogue</h1>
+      <Sidebar />
+      {/* <div className="row cards-section">
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <Cards />
+        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <Cards />
+        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <Cards />
+        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <Cards />
+        </div>
+      </div> */}
+      
     </div>
   );
 }
