9aaf691ab4f40841845f36a2eb9d9983b6a19906
Keshaw08 <keshawsoni08@gmail.com>
2023-11-02 00:59:42 +0530

    added filtering functionality

--- retro-books/src/Components/Pages/Buyer/FirstPage.js
+++ retro-books/src/Components/Pages/Buyer/FirstPage.js
@@ -1,84 +1,325 @@
+// import React, { useState, useEffect } from "react";
+// import Topbar from "../../Topbar/Topbar";
+// import Cards from "../../Book_Cards/Cards";
+// import "./FirstPage.css";
+// import Sidebar from "../../Sidebar/Sidebar";
+// // import books from "../../Book_Cards/Books";
+// import axios from "axios";
+// // import { useUser } from "../../Registration_Login/UserContext";
+
+// export default function FirstPage() {
+//   const [books, setBooks] = useState([]);
+//   const [searchedBook, setSearchedBook] = useState(null); // This will hold the searched book
+
+//   // const { userId } = useUser();
+
+//   useEffect(() => {
+//     async function fetchBooks() {
+//       try {
+//         const response = await axios.get("http://localhost:5000/get-books");
+//         setBooks(response.data);
+//         // console.log(response.data);
+//       } catch (error) {
+//         console.error("Error fetching books:", error);
+//       }
+//     }
+
+//     fetchBooks();
+//   }, []);
+
+//   const searchBookById = (bookName) => {
+//     const foundBook = books.find(
+//       //search about which algorithm does find uses.
+//       (book) => book.title.toLowerCase() === bookName.toLowerCase()
+//     );
+
+//     if (foundBook) {
+//       setSearchedBook(foundBook);
+//     } else {
+//       setSearchedBook(null); // Reset searchedBook if not found
+//     }
+
+//     console.log("search book : ", bookName);
+//   };
+//   // const userDataString = localStorage.getItem("user");
+//   // var userData;
+//   // if (userDataString) {
+//   //   userData = JSON.parse(userDataString);
+//   //   console.log("User Email:", userData.email);
+//   // }
+//   console.log(books);
+
+//   // const [filteredBooks, setFilteredBooks] = useState("");
+
+//   // const onFilterChange = (filterCriteria) => {
+//   //   const { minPrice, maxPrice } = filterCriteria;
+
+//   //   // Apply the price filter to the book data
+//   //   const filteredBooks = books.filter((book) => {
+//   //     const bookPrice = parseFloat(book.price);
+//   //     if (
+//   //       (minPrice === "" || bookPrice >= minPrice) &&
+//   //       (maxPrice === "" || bookPrice <= maxPrice)
+//   //     ) {
+//   //       return true;
+//   //     }
+//   //     return false;
+//   //   });
+
+//   //   // Update the filtered books state
+//   //   // setFilteredBooks(filteredBooks);
+//   //   setBooks(filteredBooks)
+//   // };
+
+//   // const onFilterChange = (filterCriteria) => {
+//   //   const { minPrice, maxPrice, language, location } = filterCriteria;
+
+//   //   // Apply the filters to the book data
+//   //   const filteredBooks = books.filter((book) => {
+//   //     const bookPrice = parseFloat(book.price);
+
+//   //     // Price filter
+//   //     const isPriceValid =
+//   //       (minPrice === "" || bookPrice >= minPrice) &&
+//   //       (maxPrice === "" || bookPrice <= maxPrice);
+
+//   //     // Language filter
+//   //     // const isLanguageValid =
+//   //     //   language === "" ||
+//   //     //   book.language.toLowerCase() === language.toLowerCase();
+//   //     const isLanguageValid =
+//   //       language === "" || new RegExp(language, "i").test(book.language);
+
+//   //     const isLocationValid =
+//   //       location === "" ||
+//   //       book.location.toLowerCase() === location.toLowerCase();
+
+//   //     // Return true only if all filters match
+//   //     return isPriceValid && isLanguageValid && isLocationValid;
+//   //   });
+
+//   //   // Update the filtered books state
+//   //   setBooks(filteredBooks);
+//   // };
+
+//   const onFilterChange = (filterCriteria) => {
+//     const { minPrice, maxPrice, language, location } = filterCriteria;
+
+//     // Apply the filters to the book data
+//     const filteredBooks = books.filter((book) => {
+//       const bookPrice = parseFloat(book.price);
+
+//       // Price filter
+//       const isPriceValid =
+//         (minPrice === "" || bookPrice >= minPrice) &&
+//         (maxPrice === "" || bookPrice <= maxPrice);
+
+//       // Language filter
+//       const isLanguageValid =
+//         language === "" ||
+//         book.language.toLowerCase() === language.toLowerCase();
+
+//       // const isLocationValid =
+//       //   location === "" ||
+//       //   book.location.toLowerCase() === location.toLowerCase();
+//       //   console.log("isLocation Valid : ", isLocationValid)
+
+//       return isPriceValid || isLanguageValid;
+//     });
+
+//     // Update the filtered books state
+//     setBooks(filteredBooks);
+//   };
+
+//   // console.log("filtered books are : ",filteredBooks);
+
+//   return (
+//     <div>
+//       <Topbar searchFunction={searchBookById} />
+//       <div className="row">
+//         <div className="col-lg-1 col-md-1 col-sm-1">
+//           <Sidebar onFilterChange={onFilterChange} />
+//         </div>
+//         <div className="col-lg-11 col-md-11 col-sm-11">
+//           <div className="cards-section">
+//             <div className="row">
+//               {searchedBook ? (
+//                 <div className="col-lg-4 col-md-6 col-sm-12 card-books">
+//                   <Cards
+//                     bookId={searchedBook._id}
+//                     title={searchedBook.title}
+//                     author={searchedBook.author}
+//                     language={searchedBook.language}
+//                     price={searchedBook.price}
+//                     img={`http://localhost:5000/${searchedBook.bookImage}`}
+//                     posted_by={searchedBook.posted_by}
+//                     isbn={searchedBook.isbn}
+//                   />
+//                 </div>
+//               ) : (
+//                 books.map((x) => (
+//                   <div className="col-lg-4 col-md-6 col-sm-12 card-books">
+//                     <Cards
+//                       bookId={x._id}
+//                       title={x.title}
+//                       author={x.author}
+//                       language={x.language}
+//                       price={x.price}
+//                       img={`http://localhost:5000/${x.bookImage}`}
+//                       posted_by={x.posted_by}
+//                       isbn={x.isbn}
+//                     />
+//                   </div>
+//                 ))
+//               )}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       {/* <p>{"id : " + userData.email}</p> */}
+//     </div>
+//   );
+// }
+
 import React, { useState, useEffect } from "react";
 import Topbar from "../../Topbar/Topbar";
 import Cards from "../../Book_Cards/Cards";
 import "./FirstPage.css";
 import Sidebar from "../../Sidebar/Sidebar";
-// import books from "../../Book_Cards/Books";
 import axios from "axios";
-// import { useUser } from "../../Registration_Login/UserContext";
 
 export default function FirstPage() {
   const [books, setBooks] = useState([]);
-  const [searchedBook, setSearchedBook] = useState(null); // This will hold the searched book
+  const [searchedBook, setSearchedBook] = useState(null);
+  const [filterCriteria, setFilterCriteria] = useState({
+    minPrice: "",
+    maxPrice: "",
+    language: "",
+  });
 
-  // const { userId } = useUser();
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/get-books");
+      setBooks(response.data);
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+  };
 
   useEffect(() => {
-    async function fetchBooks() {
-      try {
-        const response = await axios.get("http://localhost:5000/get-books");
-        setBooks(response.data);
-        // console.log(response.data);
-      } catch (error) {
-        console.error("Error fetching books:", error);
-      }
-    }
-
     fetchBooks();
   }, []);
 
   const searchBookById = (bookName) => {
     const foundBook = books.find(
-      //search about which algorithm does find uses.
       (book) => book.title.toLowerCase() === bookName.toLowerCase()
     );
 
     if (foundBook) {
       setSearchedBook(foundBook);
     } else {
-      setSearchedBook(null); // Reset searchedBook if not found
+      setSearchedBook(null);
     }
-
-    console.log("search book : ", bookName);
   };
-  // const userDataString = localStorage.getItem("user");
-  // var userData;
-  // if (userDataString) {
-  //   userData = JSON.parse(userDataString);
-  //   console.log("User Email:", userData.email);
-  // }
-  console.log(books);
-
-  // const [filteredBooks, setFilteredBooks] = useState("");
 
   const onFilterChange = (filterCriteria) => {
-    const { minPrice, maxPrice } = filterCriteria;
-
-    // Apply the price filter to the book data
-    const filteredBooks = books.filter((book) => {
-      const bookPrice = parseFloat(book.price);
-      if (
-        (minPrice === "" || bookPrice >= minPrice) &&
-        (maxPrice === "" || bookPrice <= maxPrice)
-      ) {
-        return true;
-      }
-      return false;
-    });
-
-    // Update the filtered books state
-    // setFilteredBooks(filteredBooks);
-    setBooks(filteredBooks)
+    const { minPrice, maxPrice, language } = filterCriteria;
+    const filteredBooks = filterBooks(books, filterCriteria);
+    console.log("Filter criteria:", filterCriteria);
+    setBooks(filteredBooks);
+    setFilterCriteria(filterCriteria);
   };
 
-  // console.log("filtered books are : ",filteredBooks);
+  const onClearFilters = () => {
+    setFilterCriteria({
+      minPrice: "",
+      maxPrice: "",
+      language: "",
+      location: "",
+    });
+    fetchBooks();
+  };
+
+  // const filterBooks = (books, filterCriteria) => {
+  //   const { minPrice, maxPrice, language } = filterCriteria;
+  //   return books.filter((book) => {
+  //     const bookPrice = parseFloat(book.price);
+  //     const isPriceValid =
+  //       (minPrice === "" || bookPrice >= minPrice) &&
+  //       (maxPrice === "" || bookPrice <= maxPrice);
+  //     console.log("price is : ",isPriceValid);
+  //     const isLanguageValid =
+  //       language === "" ||
+  //       book.language.toLowerCase() === language.toLowerCase();
+  //     return (
+  //       isPriceValid || isLanguageValid
+  //     ); // Both price and language should match
+  //   });
+  // };
+  const filterBooks = (books, filterCriteria) => {
+    const { minPrice, maxPrice, language } = filterCriteria;
+
+    // Case 3: Both price and language filters are provided
+    if (minPrice !== "" && maxPrice !== "" && language !== "") {
+      return books.filter((book) => {
+        const bookPrice = parseFloat(book.price);
+        const isPriceValid = bookPrice >= minPrice && bookPrice <= maxPrice;
+        const isLanguageValid =
+          book.language.toLowerCase() === language.toLowerCase();
+        return isPriceValid && isLanguageValid;
+      });
+    }
+
+    // // Case 1: Only price filter is provided
+    // if (minPrice !== "" && maxPrice !== "" && language === "") {
+    //   return books.filter((book) => {
+    //     const bookPrice = parseFloat(book.price);
+    //     return bookPrice >= minPrice && bookPrice <= maxPrice;
+    //   });
+    // }
+
+    // Case 1: Only price filter is provided
+    if (minPrice !== "" && maxPrice !== "" && language === "") {
+      return books.filter((book) => {
+        const bookPrice = parseFloat(book.price);
+        return bookPrice >= minPrice && bookPrice <= maxPrice;
+      });
+    }
+
+    // // Case 2: Only language filter is provided
+    // if (minPrice === "" && maxPrice === "" && language !== "") {
+    //   return books.filter(
+    //     (book) => book.language.toLowerCase() === language.toLowerCase()
+    //   );
+    // }
+    // Case 2: Only language filter is provided
+    console.log("Selected language:", language);
+
+    if (minPrice === "" && maxPrice === "" && language !== "") {
+      const filteredBooks = books.filter((book) => {
+        const isLanguageValid =
+          book.language.toLowerCase() === language.toLowerCase();
+        return isLanguageValid;
+      });
+
+      console.log("Filtered books by language:", filteredBooks);
+      return filteredBooks;
+    }
+
+    // Case 4: No filters are provided, return all books
+    return books;
+  };
 
   return (
     <div>
       <Topbar searchFunction={searchBookById} />
       <div className="row">
         <div className="col-lg-1 col-md-1 col-sm-1">
-          <Sidebar onFilterChange={onFilterChange}/>
+          <Sidebar
+            onFilterChange={onFilterChange}
+            onClearFilters={onClearFilters}
+            filterCriteria={filterCriteria}
+          />
         </div>
         <div className="col-lg-11 col-md-11 col-sm-11">
           <div className="cards-section">
@@ -116,7 +357,6 @@ export default function FirstPage() {
           </div>
         </div>
       </div>
-      {/* <p>{"id : " + userData.email}</p> */}
     </div>
   );
 }
--- retro-books/src/Components/Sidebar/Sidebar.jsx
+++ retro-books/src/Components/Sidebar/Sidebar.jsx
@@ -1,18 +1,183 @@
+// import React, { useState } from "react";
+// import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+// import "./Sidebar.css";
+// import { BsFilterSquare } from "react-icons/bs";
+
+// function Sidebar({ onFilterChange }) {
+//   // const [minPrice, setMinPrice] = useState("");
+//   // const [maxPrice, setMaxPrice] = useState("");
+
+//   // const handleMinPriceChange = (event) => {
+//   //   setMinPrice(event.target.value);
+//   // };
+
+//   // const handleMaxPriceChange = (event) => {
+//   //   setMaxPrice(event.target.value);
+//   // };
+
+//   // const applyPriceFilter = () => {
+//   //   onFilterChange({
+//   //     minPrice: parseFloat(minPrice),
+//   //     maxPrice: parseFloat(maxPrice),
+//   //   });
+//   // };
+//   return (
+//     <div>
+//       <div className="filter-button">
+//         <BsFilterSquare
+//           data-bs-toggle="offcanvas"
+//           data-bs-target="#offcanvasExample"
+//           aria-controls="offcanvasExample"
+//         />
+//       </div>
+//       <div
+//         className="offcanvas offcanvas-start"
+//         tabindex="-1"
+//         id="offcanvasExample"
+//         aria-labelledby="offcanvasExampleLabel"
+//       >
+//         <div className="offcanvas-header">
+//           <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+//             Apply Filters
+//           </h5>
+//           <button
+//             type="button"
+//             className="btn-close"
+//             data-bs-dismiss="offcanvas"
+//             aria-label="Close"
+//           ></button>
+//         </div>
+//         <div className="offcanvas-body"></div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
+
+// import React, { useState } from "react";
+// import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+// import "./Sidebar.css";
+// import { BsFilterSquare } from "react-icons/bs";
+
+// function Sidebar({ onFilterChange }) {
+//   const [minPrice, setMinPrice] = useState("");
+//   const [maxPrice, setMaxPrice] = useState("");
+//   const [selectedLanguage, setSelectedLanguage] = useState("");
+//   const [location, setLocation] = useState("");
+
+//   const handleMinPriceChange = (event) => {
+//     setMinPrice(event.target.value);
+//   };
+
+//   const handleMaxPriceChange = (event) => {
+//     setMaxPrice(event.target.value);
+//   };
+
+//   const handleLanguageChange = (event) => {
+//     setSelectedLanguage(event.target.value);
+//   };
+
+//   const handleLocationChange = (event) => {
+//     setLocation(event.target.value);
+//   };
+
+//   const applyFilters = () => {
+//     onFilterChange({
+//       minPrice: parseFloat(minPrice),
+//       maxPrice: parseFloat(maxPrice),
+//       language: selectedLanguage,
+//       location: location,
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <div className="filter-button">
+//         <BsFilterSquare
+//           data-bs-toggle="offcanvas"
+//           data-bs-target="#offcanvasExample"
+//           aria-controls="offcanvasExample"
+//         />
+//       </div>
+//       <div
+//         className="offcanvas offcanvas-start"
+//         tabIndex="-1"
+//         id="offcanvasExample"
+//         aria-labelledby="offcanvasExampleLabel"
+//       >
+//         <div className="offcanvas-header">
+//           <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+//             Apply Filters
+//           </h5>
+//           <button
+//             type="button"
+//             className="btn-close"
+//             data-bs-dismiss="offcanvas"
+//             aria-label="Close"
+//           ></button>
+//         </div>
+//         <div className="offcanvas-body">
+//           <div className="filter-section">
+//             <h6>Price Range:</h6>
+//             <div>
+//               <label htmlFor="minPrice">Min Price:</label>
+//               <input
+//                 type="number"
+//                 id="minPrice"
+//                 value={minPrice}
+//                 onChange={handleMinPriceChange}
+//               />
+//             </div>
+//             <div>
+//               <label htmlFor="maxPrice">Max Price:</label>
+//               <input
+//                 type="number"
+//                 id="maxPrice"
+//                 value={maxPrice}
+//                 onChange={handleMaxPriceChange}
+//               />
+//             </div>
+//           </div>
+//           <div className="filter-section">
+//             <h6>Language:</h6>
+//             <select
+//               value={selectedLanguage}
+//               onChange={handleLanguageChange}
+//             >
+//               <option value="">All</option>
+//               <option value="English">English</option>
+//               <option value="Hindi">Hindi</option>
+//             </select>
+//           </div>
+//           <div className="filter-section">
+//             <h6>Location:</h6>
+//             <input
+//               type="text"
+//               placeholder="Location"
+//               value={location}
+//               onChange={handleLocationChange}
+//             />
+//           </div>
+//           <button onClick={applyFilters}>Apply Filters</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
+
 import React, { useState } from "react";
-import SideNav, {
-  Toggle,
-  Nav,
-  NavItem,
-  NavIcon,
-  NavText,
-} from "@trendmicro/react-sidenav";
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 import "./Sidebar.css";
-import t from "../Assets/t.png";
+import { BsFilterSquare } from "react-icons/bs";
 
-function Sidebar({ onFilterChange }) {
+function Sidebar({ onFilterChange, onClearFilters }) {
   const [minPrice, setMinPrice] = useState("");
   const [maxPrice, setMaxPrice] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+  const [location, setLocation] = useState("");
 
   const handleMinPriceChange = (event) => {
     setMinPrice(event.target.value);
@@ -22,66 +187,163 @@ function Sidebar({ onFilterChange }) {
     setMaxPrice(event.target.value);
   };
 
-  const applyPriceFilter = () => {
+  const handleLanguageChange = (value) => {
+    console.log("Selected language:", value);
+    setSelectedLanguage(value); // Assuming 'setSelectedLanguage' is the state update function
+  };
+
+  const applyFilters = () => {
+    // Parse minPrice and maxPrice only if they are non-empty strings
+    const minPriceValue = minPrice ? parseFloat(minPrice) : "";
+    const maxPriceValue = maxPrice ? parseFloat(maxPrice) : "";
+
     onFilterChange({
-      minPrice: parseFloat(minPrice),
-      maxPrice: parseFloat(maxPrice),
+      minPrice: minPriceValue,
+      maxPrice: maxPriceValue,
+      language: selectedLanguage,
     });
   };
+
+  const clearFilters = () => {
+    // Clear all filter fields and show all books
+    setMinPrice("");
+    setMaxPrice("");
+    setSelectedLanguage("");
+    onClearFilters();
+  };
+
   return (
     <div>
-      <SideNav
-        onSelect={(selected) => {
-          // Add your code here
-        }}
+      <div className="filter-button">
+        <BsFilterSquare
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasExample"
+          aria-controls="offcanvasExample"
+        />
+      </div>
+      <div
+        className="offcanvas offcanvas-start"
+        tabIndex="-1"
+        id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel"
       >
-        <SideNav.Toggle />
-        <SideNav.Nav defaultSelected="home">
-          <NavItem eventKey="home">
-            <NavIcon>
-              <i className="fa fa-fw fa-home" style={{ fontSize: "1.75em" }} />
-            </NavIcon>
-            <NavText className="side-nav-text">Home</NavText>
-          </NavItem>
-          <NavItem eventKey="home">
-            <NavIcon>
-              <i className="fa fa-fw fa-home" style={{ fontSize: "1.75em" }} />
-            </NavIcon>
-            <NavText className="side-nav-text">Wishlist</NavText>
-          </NavItem>
-          <NavItem eventKey="home">
-            <NavIcon>
-              <i className="fa fa-fw fa-home" style={{ fontSize: "1.75em" }} />
-            </NavIcon>
-            <NavText className="side-nav-text">Price Filter</NavText>
-            {/* <div className="price-filter-inputs">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+            Apply Filters
+          </h5>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body">
+          <div className="filter-section">
+            <h6>Price Range:</h6>
+            {/* <div className="form__group field">
               <input
                 type="number"
-                placeholder="Min Price"
+                id="minPrice"
                 value={minPrice}
                 onChange={handleMinPriceChange}
+                className="form__field"
+                placeholder="Min Price"
               />
+              <label htmlFor="minPrice" className="form__label">
+                Min Price:
+              </label>
+            </div>  */}
+            <div className="form__group field">
               <input
                 type="number"
-                placeholder="Max Price"
+                id="minPrice"
+                value={minPrice}
+                onChange={handleMinPriceChange}
+                className="form__field"
+                placeholder="Min Price"
+              />
+              <label htmlFor="minPrice" className="form__label">
+                Min Price:
+              </label>
+            </div>
+            <div className="form__group field">
+              <input
+                type="number"
+                id="maxPrice"
                 value={maxPrice}
                 onChange={handleMaxPriceChange}
+                className="form__field"
+                placeholder="Min Price"
               />
-              <button onClick={applyPriceFilter}>Apply</button>
-            </div> */}
-          </NavItem>
-          {/* <NavItem eventKey="charts">
-            <NavIcon>
-              <i
-                className="fa fa-fw fa-line-chart"
-                style={{ fontSize: "1.75em" }}
-              />
-            </NavIcon>
-            <NavText className="side-nav-text">Charts</NavText>
-          
-          </NavItem> */}
-        </SideNav.Nav>
-      </SideNav>
+              <label htmlFor="maxPrice" className="form__label">
+                Max Price:
+              </label>
+            </div>
+          </div>
+          <div className="filter-section">
+            <h6>Choose Language:</h6>
+
+            <div className="dropdown drop-box-button">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {selectedLanguage || "All"}
+              </button>
+              <ul className="dropdown-menu">
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => handleLanguageChange("")}
+                  >
+                    All
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => handleLanguageChange("English")}
+                  >
+                    English
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => handleLanguageChange("Hindi")}
+                  >
+                    Hindi
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div
+            className="btn-group apply-clear-button"
+            role="group"
+            aria-label="Basic example"
+          >
+            <button
+              type="button"
+              className="btn btn-outline-success"
+              onClick={applyFilters}
+            >
+              Apply
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-info"
+              onClick={clearFilters}
+            >
+              Clear
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
