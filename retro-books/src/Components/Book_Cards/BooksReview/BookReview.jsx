a07425538fa5d242a74fe81904369bb5c2d1e341
Keshaw08 <keshawsoni08@gmail.com>
2023-12-11 20:07:58 +0530

    added data

--- retro-books/src/Components/Book_Cards/BooksReview/BookReview.jsx
+++ retro-books/src/Components/Book_Cards/BooksReview/BookReview.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from "react";
 
 function BookReview(props) {
-  const [reviewName, setReviewName] = useState("");
+  // const [reviewName, setReviewName] = useState("");
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/api/current-user?email=${props.email}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setReviewName(data.name);
-      })
-      .catch((error) => {
-        console.error("Error fetching current user:", error);
-      });
-  }, [props.email]);
+  // useEffect(() => {
+  //   fetch(`http://localhost:5000/api/current-user?email=${props.email}`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setReviewName(data.name);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching current user:", error);
+  //     });
+  // }, [props.email]);
 
   return (
     <div>
-      <strong>{reviewName}</strong>
+      {/* <strong>{reviewName}</strong> */}
+      <strong>{props.email}</strong>
       <p>{props.reviewText}</p>
     </div>
   );
